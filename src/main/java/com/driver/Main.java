23a0629fe34b5cec110448d4a80402578be92e8b
naushad <officalacc827151@gmail.com>
2023-01-13 17:30:38 +0530

    psvm bhul gaya tha isse phle

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,28 +1,33 @@
 package com.driver;
 
 public class Main {
-    Product p = new Product();
+    public static void main(String[] args) {
 
-    int ansValue = p.product(2,3);
+        Product p = new Product();
 
-    int secondAnswerValue=p.product(2,3,4);
+        int ansValue = p.product(2,3);
 
-    double thirdAnswerValue=p.product(2.1,2.9);
+        int secondAnswerValue=p.product(2,3,4);
+
+        double thirdAnswerValue=p.product(2.1,2.9);
+
+    }
+
 
 }
 class Product{
     public int product(int x, int y) {
-        int ans=x+y;
+        int ans=x*y;
         return ans;
     }
     public int product(int x, int y, int z) {
 
-        int secondAnswer=x+y+z;
+        int secondAnswer=x*y*z;
         return  secondAnswer;
     }
 
     public double product(double x, double y) {
-        double thirdAnswer=x+y;
+        double thirdAnswer=x*y;
         return thirdAnswer;
     }
 }
